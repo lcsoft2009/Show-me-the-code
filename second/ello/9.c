fb8d69967007775f2c6379d0d96b17ecf1a1558a
ellochen <silver.accc@gmail.com>
2013-03-19 00:15:47 +0800

    just add file

---
+++ second/ello/9.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 9.c
+	> Author: ellochen
+	> Mail: god_mode@yeah.net
+	> Created Time: 2013年03月18日 22时49分16秒 CST
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main()
+{
+
+	return 0;
+}
